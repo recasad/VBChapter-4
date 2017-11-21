3100136514a5165f0c2bdc1a560f352d05f356dd
recasad <33033835+recasad@users.noreply.github.com>
2017-11-21 11:35:33 -0600

    Update Chapter 4

--- Larger and Smaller/Larger and Smaller/Form1.vb
+++ Larger and Smaller/Larger and Smaller/Form1.vb
@@ -1,11 +1,38 @@
 ﻿Public Class Form1
 
     Private Sub btnCompare_Click(sender As Object, e As EventArgs) Handles btnCompare.Click
+        ' Dim integer input as integer.
         Dim intA As Integer
         Dim intB As Integer
 
-        If Integer.TryParse(txtIntegerA, intA) Then
-            If Int() Then
+        ' Check if integers, and display greater/smaller/equal messages.
+        If Integer.TryParse(txtIntegerA.Text, intA) Then
+            If Integer.TryParse(txtIntegerB.Text, intB) Then
+                If intA > intB Then
+                    lblResult.Text = "Integer A is greatest."
+                ElseIf intB > intA Then
+                    lblResult.Text = "Integer B is greatest."
+                ElseIf intA = intB Then
+                    lblResult.Text = "Integers are equal."
+                End If
+            Else
+                lblResult.Text = "Please input a numerical integer."
             End If
+        Else
+            lblResult.Text = "Please input a numerical integer."
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the textboxes and label.
+        txtIntegerA.Clear()
+        txtIntegerB.Clear()
+        lblResult.Text = String.Empty
+        txtIntegerA.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form.
+        Me.Close()
     End Sub
 End Class
---
+++ Roman Numeral Converter/Roman Numeral Converter/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Select Case CInt(txtDecimal.Text)
+            Case 1
+                lblNumerals.Text = "I"
+            Case 2
+                lblNumerals.Text = "II"
+            Case 3
+                lblNumerals.Text = "III"
+            Case 4
+                lblNumerals.Text = "IV"
+            Case 5
+                lblNumerals.Text = "V"
+            Case 6
+                lblNumerals.Text = "VI"
+            Case 7
+                lblNumerals.Text = "VII"
+            Case 8
+                lblNumerals.Text = "VIII"
+            Case 9
+                lblNumerals.Text = "IX"
+            Case 10
+                lblNumerals.Text = "X"
+            Case Else
+                lblNumerals.Text = "Please input a valid number"
+        End Select
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Me.Close()
+    End Sub
+End Class
