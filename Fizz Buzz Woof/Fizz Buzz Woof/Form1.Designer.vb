cebdec8536e9592374bcf0c5ea5c03b11837d66c
recasad <rc308750@students.davenport.k12.ia.us>
2017-11-27 22:45:34 -0600

    Update Fizz Buzz Woof

---
+++ Fizz Buzz Woof/Fizz Buzz Woof/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.lstResults = New System.Windows.Forms.ListBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(151, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Please input numerical integer:"
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(186, 31)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber.TabIndex = 1
+        '
+        'lstResults
+        '
+        Me.lstResults.FormattingEnabled = True
+        Me.lstResults.Location = New System.Drawing.Point(32, 109)
+        Me.lstResults.Name = "lstResults"
+        Me.lstResults.Size = New System.Drawing.Size(253, 238)
+        Me.lstResults.TabIndex = 2
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(121, 69)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 3
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(202, 69)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(40, 69)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(320, 395)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lstResults)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Fizz Buzz Woof"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents lstResults As ListBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ Fizz Buzz Woof/Fizz Buzz Woof/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        Dim intNumber As Integer = txtNumber.Text
+        Dim intCount As Integer = 1
+
+        Do Until intCount = (intNumber + 1)
+            If intCount Mod 3 = 0 Then
+                If intCount Mod 5 = 0 Then
+                    If intCount Mod 7 = 0 Then
+                        lstResults.Items.Add("Fizz Buzz Woof")
+                    Else
+                        lstResults.Items.Add("Fizz Buzz")
+                    End If
+                Else
+                    lstResults.Items.Add("Fizz")
+                End If
+            ElseIf intCount Mod 7 = 0 Then
+                If intCount Mod 5 = 0 Then
+                    lstResults.Items.Add("Buzz Woof")
+                ElseIf intCount Mod 3 = 0 Then
+                    lstResults.Items.Add("Fizz Woof")
+                Else
+                    lstResults.Items.Add("Woof")
+                End If
+            ElseIf intCount Mod 5 = 0 Then
+                lstResults.Items.Add("Buzz")
+            Else
+                lstResults.Items.Add(intCount)
+            End If
+            intCount += 1
+        Loop
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lstResults.Items.Clear()
+        txtNumber.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
